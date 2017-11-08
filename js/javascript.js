35c3a204fd6a66a2788501db4dc1d05b7c2c7b07
IrinaZiborova <irisha.ziborova@gmail.com>
2017-11-08 21:05:05 +0100

    my site
    1

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+//$(function(){
+//	var color = ['#000','#fff','#000'];
+//	var i = 0;
+//	$('#left').click(function(){
+//		if (i < color.length){
+//			i++;
+//			$(this).animate({ top: '-=500px' }, 600, 'easeOutCirc', function () {
+//				$(this).css('background-color', color[i]);
+//			});
+//		}
+//	});
+//});
+
+//elem.scrollTop += 500
